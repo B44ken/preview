eb3424e5b75f5848f33f4ec8cfe74d6223dae79c
b44ken <bradboratto@gmail.com>
2020-05-31 13:58:47 -0400

    IDK BUT IT WORKS 2 MINS LEFT WHUIRFHWEFGUIEHWG

--- src/main.js
+++ src/main.js
@@ -13,37 +13,41 @@ widget.style.left = "50px"
 document.body.appendChild(widget)
 
 document.body.addEventListener("mousemove", event => {
-    y=event.clientY
+	y=event.clientY
     x=event.clientX
     widget.style.left=(x+5)+"px"
     widget.style.top=(y+5)+"px"
-  })
+})
 
 document.body.addEventListener("mouseover", event => {
-  widget.style.display = "none"
+	
+	widget.style.display = "none"
+	
+	const onlyLinks = event.path.filter(e => e.nodeName == "A");
+	if (onlyLinks.length == 0) return false;
+	
+	const url = new URL(onlyLinks[0]);
+	const hostname = new URL(onlyLinks[0]).hostname;
+	const chost = new URL(window.location.href).hostname;
+	if (chost == hostname) return false
 
-  const onlyLinks = event.path.filter(e => e.nodeName == "A");
-  if (onlyLinks.length == 0) return false;
-  
-  const url = new URL(onlyLinks[0]);
-  const hostname = new URL(onlyLinks[0]).hostname;
-  const chost = new URL(window.location.href).hostname;
-  if (!scrapers[hostname]) return false
-  if (chost == hostname) return false
-
-  widget.style.display = "block"
-  
-  fetch("https://api.allorigins.win/get?url=" + url.href)
-	  .then(e => e.json())
-	  .then(html => {
-		  dom = new DOMParser().parseFromString(html.contents, "text/html")
-		  var code = false
-		  for(scraper in scrapers) {
-			  if(scraper[0].test(html.href)) 
-				  code = scraper[1](dom, url.href)
-				  widget.innerHTML = code
-		  }
-		  if(code != false) { s }
-		  if(code == false) widget.style.display = "none"
-  })
+	var validScraper = false
+	for(i in scrapers) {
+		console.log(scrapers)
+		if(scrapers[i][0].test(url.href)) validScraper = scrapers[i][1]
+	}
+	if(validScraper == false) return false
+	
+	widget.style.display = "block"
+	
+	console.log("help")
+	fetch("https://api.allorigins.win/get?url=" + url.href)
+	.then(e => e.json())
+	.then(html => {
+		dom = new DOMParser().parseFromString(html.contents, "text/html")
+		var code = false
+		code = validScraper(dom, url.href)
+		if(code != false) widget.innerHTML = code
+		if(code == false) widget.style.display = "none"
+	})
 })
--- src/scrapers.js
+++ src/scrapers.js
@@ -1,11 +1,11 @@
 /* individual scrapers */
-const scrapers = {}
+var scrapers = []
 
-scrapers.push([/youtube\.com\/watch\?v=[A-z1-9]+/, (dom, url) => {
+scrapers.push([new RegExp(/youtube\.com\/watch\?v=[A-z1-9]+/), (dom, url) => {
 	const videoID = url.split("watch?v=")
 	return `<iframe src="https://youtube.com/embed/${videoID[1]}/?autoplay=1"></iframe>`
 }])
-scrapers.push([/en\.wikipedia\.org\/wiki\/[A-z1-9_]+/, (dom) => {
+scrapers.push([new RegExp(/en\.wikipedia\.org\/wiki\/[A-z1-9_]+/), (dom) => {
     const title = dom.querySelector("#firstHeading").innerText
     const contents = dom.querySelector("#toc").innerText.replace(/(\r\n|\n|\r)/gm,"<br>")
 
@@ -14,7 +14,7 @@ scrapers.push([/en\.wikipedia\.org\/wiki\/[A-z1-9_]+/, (dom) => {
 	<p>${contents}</p>
 	`
 }])
-scrapers.push([/github\.com\/[A-z1-9]+\/[A-z1-9]+$/, (dom) => {
+scrapers.push([new RegExp(/github\.com\/[A-z1-9]+\/[A-z1-9]+$/), (dom) => {
     const author = dom.querySelector(".url.fn").innerText
     const title = dom.querySelector(".mr-2.flex-self-stretch").innerHTML
 	const desc = dom.querySelector(".text-gray-dark.mr-2").innerText
